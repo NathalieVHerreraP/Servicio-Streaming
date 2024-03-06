f6960b69c6863110d7973bcfbd3e70988d3bc6ce
Nath Herrera <nathalieherrera0108@gmail.com>
2024-03-06 11:33:09 -0600

    get por id

--- streaming/src/componentes/PeliculaInfo.js
+++ streaming/src/componentes/PeliculaInfo.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-function PeliculasInfo(){
+function PeliculaInfo(props){
+
+    const [pelicula,setPelicula] = React.useState(null);
+
+    React.useEffect(() => {
+        fetch(`/api/pelicula/:${props.id}`)
+        .then((res) => res.json())
+        .then((pelicula) => setPelicula(pelicula))
+        .catch(error => {
+            console.error(error);
+          });
+    }, []);
+
+    console.log(pelicula);
 
     return (
         <div>
-            
+
         </div>
     );
 }
 
-export default PeliculasInfo;
+export default PeliculaInfo;
