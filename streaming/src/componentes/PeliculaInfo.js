9d74a54eb5fd9a61be435b20a1321e6a25dfd133
Nath Herrera <nathalieherrera0108@gmail.com>
2024-03-06 21:57:46 -0600

    Pelicula info

--- streaming/src/componentes/PeliculaInfo.js
+++ streaming/src/componentes/PeliculaInfo.js
@@ -1,11 +1,14 @@
 import React from "react";
+import {useParams} from 'react-router-dom';
+import "../css/peliculaInfo.css";
 
-function PeliculaInfo(props){
 
+function PeliculaInfo(){
+    const params = useParams();
     const [pelicula,setPelicula] = React.useState(null);
 
     React.useEffect(() => {
-        fetch(`/api/pelicula/:${props.id}`)
+        fetch(`/api/pelicula/${params.id}`)
         .then((res) => res.json())
         .then((pelicula) => setPelicula(pelicula))
         .catch(error => {
@@ -15,9 +18,31 @@ function PeliculaInfo(props){
 
     console.log(pelicula);
 
+    let peliculaInfo = (
+        <div className="pelicula-info">
+                <h2>{pelicula.titulo} {pelicula.anio}</h2>
+                <img 
+                    src={pelicula.poster} 
+                    title={`Poster de la pelicula ${pelicula.titulo}`}
+                />
+                <p>Generos: {pelicula.genero.join(", ")}</p>
+                <p>Calificación rotten tomatoes: {pelicula.calificacionRT}</p>
+            </div>
+    );
+
+    let comentariosLista = pelicula.comentarios.map((comentario) => {
+        return(
+            <div>
+                <p>{comentario.usuario} </p>
+                <p>{comentario.comentario}</p>
+            </div>
+        );
+    });
+
     return (
         <div>
-
+            {peliculaInfo}
+            {comentariosLista}
         </div>
     );
 }
--- streaming/src/componentes/Peliculas.js
+++ streaming/src/componentes/Peliculas.js
@@ -1,5 +1,6 @@
 import React from"react";
 import '../css/peliculas.css';
+import {Link} from "react-router-dom";
 
 function Peliculas (){
 
@@ -16,29 +17,38 @@ function Peliculas (){
 
     console.log(peliculas);
 
-
-    let peliculasElementos = peliculas.map(function(pelicula) {
+    let peliculasElementos = peliculas.map((pelicula) => {
         let sum = 0;
         for (let i = 0; i < pelicula.estrellas_usuarios.length; i++) {
              sum = sum + pelicula.estrellas_usuarios[i].calificacion;
             
         }
-        let avg = 0;
-        avg = sum / pelicula.estrellas_usuarios.length;
 
-        console.log(avg);
+        let avg = 0;
+        avg = sum / pelicula.estrellas_usuarios.length;   
         return( 
             <div className="peliculas-info">
-                <img src={`data:image/gif;base64,`+ peliculas.portada} alt="" />
+                <img src={peliculas.portada} alt="" />
                 <div className="peliculas-texto">
                     <p>{pelicula.titulo} {pelicula.anio}</p>
                     <p>{avg} ★</p>
-                    <button className="pelicula-icono"><img className="pelicula-icono" src={require(`../imagenes/iconoPlay.png`)} title="Ver"/></button>
-                    <button className="pelicula-icono"><img className="pelicula-icono" src={require(`../imagenes/iconoCalificar.png`)} Title="información"/></button>
+                    <Link>
+                    <img 
+                        className="pelicula-icono" 
+                        src={require(`../imagenes/iconoPlay.png`)} 
+                        title="Ver"/>
+                    </Link>
+                    <Link to={"/PeliculaInfo/" + pelicula._id}>
+                    <img 
+                        className="pelicula-icono" 
+                        src={require(`../imagenes/iconoCalificar.png`)} 
+                        Title="información"/>
+                    </Link>
                 </div>
             </div>
-        )
-    });
+        );
+    });    
+    
 
     return(
         <div className="peliculas-contenedor">
