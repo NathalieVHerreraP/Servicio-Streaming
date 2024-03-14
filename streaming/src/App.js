590aa5363f2b308dbb55b9cbc759a0c02f3bddc6
Diego1887 <juandiegomartinezromo@gmail.com>
2024-03-13 18:20:06 -0600

    abcd

--- streaming/src/App.js
+++ streaming/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Peliculas from './componentes/Peliculas.js';
 import PeliculaInfo from './componentes/PeliculaInfo.js';
+import Home from './componentes/Home.js';
 import {Routes, Route} from "react-router-dom";
 
 function App() {
@@ -11,8 +12,8 @@ function App() {
         <Routes>
           <Route path="/Peliculas" element={<Peliculas/>}/>
           <Route path="/Series"/>
-          <Route path="/*"/>
-          <Route path=""/>
+         
+          <Route path="/Home" element={<Home/>}/>
         </Routes>
       </header>
     </div>
---
+++ streaming/src/componentes/Home.js
@@ -0,0 +1,58 @@
+import React from  "react";
+import '../css/home.css';
+// import rana from '../imagenes/rana.jpeg'
+
+function Home (){
+    
+    const [series, setSeries] = React.useState(null);
+
+    React.useEffect(() => {
+        fetch("/api/series/")
+        .then((res) => res.json())
+        .then((series) => setSeries(series))
+        .catch(error => {
+            console.error(error);
+          });
+    }, []);
+
+    console.log(series);
+
+    
+
+    return(
+        <div className="seriesInfo">
+            
+            <h1> BIENVENIDOS </h1>
+            <h2> Aquí encontrars series y peliculas de tu agrado </h2>
+            <p> Entra en nuetro menú y diviertete mucho </p>
+
+            <div className="Portadas">
+                <ul>
+                    {series?.map((series) => (
+                        <>
+                        <div className="serie">
+
+                        <div className="portadaSerie">
+                           <img src={series.portada} alt="portada"  />
+                         
+                        </div>
+                        
+
+                        </div>
+                        
+                        
+                         <br></br>
+                         </>
+                    ))}
+                    
+                    
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+
+
+
+export default Home;
